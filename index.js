1f882a1ced9ac084aa63e58308b2aaf464f769ee
One-G-Row <148204501+One-G-Row@users.noreply.github.com>
2024-03-14 10:20:10 +0300

    update intro to functions

--- index.js
+++ index.js
@@ -21,20 +21,10 @@ function logWhisper(functions) {
 }
 logWhisper(JAVASCRIPT_FUNCTIONS);
 
-/*function sayHiToHeadphonedRoomate(string) {
-  if (string.toLowerCase() === string) {
-    return "I can't hear you";
-  } else if (string.toUpperCase() === string) {
-    return "YES INDEED!";
-  } else if (string === "Let's have dinner together!") {
-    return "I would love to!";
-  }
-}*/
-
 function sayHiToHeadphonedRoommate(string) {
-  if (string.toLowerCase() === string) {
+  if (string === string.toLowerCase()) {
     return "I can't hear you!";
-  } else if (string.toUpperCase() === string) {
+  } else if (string === string.toUpperCase() ) {
     return "YES INDEED!";
   } else if (string === "Let's have dinner together!") {
     return "I would love to!";
@@ -43,20 +33,3 @@ function sayHiToHeadphonedRoommate(string) {
 
 console.log(sayHiToHeadphonedRoomate());
 
-/*//sayHiToHeadphonedRoommate.toLowerCase
-function sayHiToHeadphonedRoomateLower(string) {
-  return "I can't hear you!";
-}
-console.log(sayHiToHeadphonedRoomate("hello"));
-
-//sayHiToHeadphonedRoomate.toUpperCase
-function sayHiToHeadphonedRoomateUpper(string) {
-  return "YES INDEED!";
-}
-console.log(sayHiToHeadphonedRoomateUpper("HELLO"));
-
-//default
-function sayHiToHeadphonedRoomateDinner(string) {
-  return "I would love to!";
-}
-console.log(sayHiToHeadphonedRoomateDinner("Let's have dinner together"));*/
